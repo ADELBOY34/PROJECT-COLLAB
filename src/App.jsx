dc13ebb512885bd8015bc332bbbdbebcae6279b2
islemjsk7 <islaamadrid@gmail.com>
2026-04-20 12:16:39 +0100

    createdpageislem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import PageIslem from './pages/PageIslem'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<h1>home page</h1>}/>
+        <Route path='/islem' element={<PageIslem/>}/>
       </Routes>
     </div>
   )
