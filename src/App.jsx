4d7f5287f05d530468fa552ca406c278d7d0a4b0
2003myriam <lounesmyriam210@gmail.com>
2026-04-20 12:15:38 +0100

    create page myriam

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import PageMyriam from './pages/PageMyriam'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<h1>home page</h1>}/>
+        <Route path='/myriam' element={<PageMyriam/>}/>
       </Routes>
     </div>
   )
